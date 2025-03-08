8093266a1021bc07ba13e456cb017ff86218b080
OscarTorres <oscar03012003@gmail.com>
2025-03-08 17:12:26 +0100

    Add asistente de Ia de One goal en las paginas principales

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,32 @@ window.addEventListener("load", () => {
     }, 1000);
     
 });
+document.getElementById('asistente').addEventListener('click', function() {
+    document.getElementById('modal').classList.remove('hidden');
+});
+document.getElementById('cerrar').addEventListener('click', function() {
+    document.getElementById('modal').classList.add('hidden');
+});
+
+const asistente = document.getElementById("asistente");
+const tooltip = document.getElementById("tooltip");
+
+asistente.addEventListener("mouseenter", () => {
+    tooltip.classList.remove("opacity-0");
+});
+
+asistente.addEventListener("mouseleave", () => {
+    tooltip.classList.add("opacity-0");
+});
+
+// Simular pestañeo con cambio de imagen
+setInterval(() => {
+    let balon = document.getElementById("balon");
+    let balonModal = document.getElementById("balon-modal");
+    balon.src = "../assets/balonIaPestañeoMod.png";
+    balonModal.src = "../assets/balonIaPestañeoMod.png";
+    setTimeout(() => {
+        balon.src = "../assets/balonIaNormal.png";
+        balonModal.src = "../assets/balonIaNormal.png";
+    }, 300);
+}, 5000);
